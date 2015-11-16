f4886bd94d3892c1895f42b1321f4407ea3dadcc
Nikita <simoyanov93@gmail.com>
2015-11-17 01:49:07 +0300

    Update main.js
    change place of function

--- veeva/js/main.js
+++ veeva/js/main.js
@@ -1,35 +1,9 @@
-// Появление основных элементов
-
-$(".first_fade").delay(700).fadeIn(800);
-fadeYear(1,1400);//первый год
-
-//временная линия слайдер
-
-$(".full-width-slider").on('change',function() {
-    var val = $("#slider-12").val();
-    if (val > 1){fadeYear(val,0);}
-    if (val > 8) {$(".timeline_canvas").css("margin-left","-" + (val - 8) * 238 +"px");}
-    if (val < 9){$(".timeline_canvas").css("margin-left","0px");}
-});
-
-//всплывашка
-
-$(".popup").on('click',function(){$(".popup").fadeOut(600)});
-$(".footer_link").on('click',function(){$(".popup").fadeIn(600)});
-
 //функция появления ромба, a - номер года(от 1 до 14), b - задержка(delay)
 
 function fadeYear(a,b){
     var delayN = b;
     var i = a;
     for(a=$(".timeline_canvas_year:visible").length + 1; a <= i; a++){
-        var year;
-        if (a < 10){
-            year = ".year200" + a;
-        } else {
-            year = ".year20" + a;
-        }
-        if (a % 2 == 0){evenYear(year);}else{oddYear(year);}
         function evenYear(year){
             $(year).css("display","inline-block");
             var k = delayN;
@@ -54,8 +28,37 @@ function fadeYear(a,b){
             }
             delayN = arr[arr.length-1];
         }
+        var year;
+        if (a < 10){
+            year = ".year200" + a;
+        } else {
+            year = ".year20" + a;
+        }
+        if (a % 2 == 0){evenYear(year);}else{oddYear(year);}
         delayN = delayN + 500;
     }
 }
+
+
+
+// Появление основных элементов
+
+$(".first_fade").delay(700).fadeIn(800);
+fadeYear(1,1400);//первый год
+
+//временная линия слайдер
+
+$(".full-width-slider").on('change',function() {
+    var val = $("#slider-12").val();
+    if (val > 1){fadeYear(val,0);}
+    if (val > 8) {$(".timeline_canvas").css("margin-left","-" + (val - 8) * 238 +"px");}
+    if (val < 9){$(".timeline_canvas").css("margin-left","0px");}
+});
+
+//всплывашка
+
+$(".popup").on('click',function(){$(".popup").fadeOut(600)});
+$(".footer_link").on('click',function(){$(".popup").fadeIn(600)});
+
 
 
